9be617d4dbc5d4132cacfc874b6e9707c7256b0b
Derek <derex1987@gmail.com>
2026-05-09 16:04:04 -0500

    feat: support for 26.1 with a new listener

--- src/main/java/io/github/derec4/sleepAnimation/SleepAnimation.java
+++ src/main/java/io/github/derec4/sleepAnimation/SleepAnimation.java
@@ -14,7 +14,11 @@ public final class SleepAnimation extends JavaPlugin {
         ConfigManager.loadConfig(this);
         timeSkipper = new TimeSkipper(this, ConfigManager.getSkipSpeed(), 0);
         timeSkipper.start();
-        getServer().getPluginManager().registerEvents(new SleepListener(this), this);
+        if (shouldUseModernTimeSkipListener()) {
+            getServer().getPluginManager().registerEvents(new SleepListenerModern(this), this);
+        } else {
+            getServer().getPluginManager().registerEvents(new SleepListener(this), this);
+        }
 
         // register base command 'sleepanimation' and handle subcommands (reload)
         PluginCommand sleepCmd = getCommand("sleepanimation");
@@ -44,5 +48,57 @@ public final class SleepAnimation extends JavaPlugin {
 
     public void setTimeSkipper(TimeSkipper timeSkipper) {
         this.timeSkipper = timeSkipper;
+    }
+
+    private boolean shouldUseModernTimeSkipListener() {
+        return isPaperVersionAtLeast("26.1.2") || hasClockTimeSkipEventClass();
+    }
+
+    private boolean isPaperVersionAtLeast(String minVersion) {
+        String bukkitVersion = Bukkit.getServer().getBukkitVersion();
+        if (bukkitVersion == null) {
+            return false;
+        }
+
+        String baseVersion = bukkitVersion.split("-")[0];
+        if (!baseVersion.matches("\\d+(\\.\\d+)*")) {
+            return false;
+        }
+
+        return compareVersions(baseVersion, minVersion) >= 0;
+    }
+
+    private int compareVersions(String left, String right) {
+        String[] leftParts = left.split("\\.");
+        String[] rightParts = right.split("\\.");
+        int max = Math.max(leftParts.length, rightParts.length);
+
+        for (int i = 0; i < max; i++) {
+            int leftNum = i < leftParts.length ? parseVersionPart(leftParts[i]) : 0;
+            int rightNum = i < rightParts.length ? parseVersionPart(rightParts[i]) : 0;
+
+            if (leftNum != rightNum) {
+                return Integer.compare(leftNum, rightNum);
+            }
+        }
+
+        return 0;
+    }
+
+    private int parseVersionPart(String part) {
+        try {
+            return Integer.parseInt(part);
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
+    }
+
+    private boolean hasClockTimeSkipEventClass() {
+        try {
+            Class.forName("org.bukkit.event.world.ClockTimeSkipEvent", false, getClass().getClassLoader());
+            return true;
+        } catch (ClassNotFoundException ex) {
+            return false;
+        }
     }
 }
